9afc205b860ab4fb143dee2d7ab4d25e991591aa
Sajin25 <120718410+Sajin25@users.noreply.github.com>
2024-07-19 22:04:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function navigate(url) {
-    const overlay = document.getElementById('overlay');
-    overlay.classList.add('show');
-    setTimeout(() => {
-        window.location.href = url;
-    }, 500); // Match this duration to the CSS transition duration
+    window.location.href = url;
 }
